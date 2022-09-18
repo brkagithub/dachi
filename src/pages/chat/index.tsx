9b210c7256e9b5c5efd20cc10880dd902caf1c84
brka <mbrkic16@gmail.com>
2022-09-18 16:56:12 +0200

    renamed variable req to friend :)

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -41,24 +41,25 @@ const Home: NextPage = () => {
         <div className="max-w-2xl mx-auto pt-8 pr-4 pl-4 md:pl-2 md:pr-2">
           <h1 className="text-2xl">My chats</h1>
           <div className="mx-auto pt-4">
-            {friends?.map((req) => (
+            {friends?.map((friend) => (
               <NextLink
                 href={`chat/${
-                  (req.requestInitiatorId != meData.id &&
-                    req.requestInitiator.name) ||
-                  (req.requestTargetId != meData.id && req.requestTarget.name)
+                  (friend.requestInitiatorId != meData.id &&
+                    friend.requestInitiator.name) ||
+                  (friend.requestTargetId != meData.id &&
+                    friend.requestTarget.name)
                 }`}
-                key={req.id}
+                key={friend.id}
               >
                 <div className="flex items-center cursor-pointer border rounded-3xl border-gray-400 p-2 mb-4">
                   <div className="flex justify-start items-center w-full">
                     <img
                       className="h-16 w-16 rounded-full"
                       src={
-                        (req.requestInitiatorId != meData.id &&
-                          req.requestInitiator.image) ||
-                        (req.requestTargetId != meData.id &&
-                          req.requestTarget.image) ||
+                        (friend.requestInitiatorId != meData.id &&
+                          friend.requestInitiator.image) ||
+                        (friend.requestTargetId != meData.id &&
+                          friend.requestTarget.image) ||
                         ""
                       }
                     ></img>
@@ -67,23 +68,23 @@ const Home: NextPage = () => {
                       <div className="flex flex-col">
                         <div className="text-left underline">
                           @
-                          {(req.requestInitiatorId != meData.id &&
-                            req.requestInitiator.name) ||
-                            (req.requestTargetId != meData.id &&
-                              req.requestTarget.name)}
+                          {(friend.requestInitiatorId != meData.id &&
+                            friend.requestInitiator.name) ||
+                            (friend.requestTargetId != meData.id &&
+                              friend.requestTarget.name)}
                         </div>
 
                         <div className="text-sm text-left">
-                          {req.lastMessage[0]?.body
-                            ? req.lastMessage[0]?.body.length > 23
-                              ? req.lastMessage[0]?.body.substring(0, 23) +
+                          {friend.lastMessage[0]?.body
+                            ? friend.lastMessage[0]?.body.length > 23
+                              ? friend.lastMessage[0]?.body.substring(0, 23) +
                                 "..."
-                              : req.lastMessage[0]?.body
+                              : friend.lastMessage[0]?.body
                             : "start a conversation..."}
                         </div>
                       </div>
                       <div className="pr-1 text-sm">
-                        {req.lastMessage[0]?.timestamp
+                        {friend.lastMessage[0]?.timestamp
                           .toISOString()
                           .substring(11, 16)}
                       </div>
