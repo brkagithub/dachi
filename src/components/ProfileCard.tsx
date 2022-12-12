d64140a282c8f40e4c1d85be33c7f295494fa6b5
brka <mbrkic16@gmail.com>
2022-12-12 15:27:48 +0100

    extracted profile card logic

---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,206 @@
+import { Role, Tier } from "@prisma/client";
+import React from "react";
+import { trpc } from "../utils/trpc";
+import NextLink from "next/link";
+
+type ProfileInput = {
+  id: string;
+  name: string | null;
+  image: string | null;
+  firstName: string | null;
+  role: Role | null;
+  fav_champion1: string | null;
+  tier: Tier | null;
+};
+
+const ProfileCard: React.FC<{
+  acceptDecline: boolean;
+  profileInput: ProfileInput;
+}> = ({ acceptDecline, profileInput }) => {
+  const acceptFriendReqMutation = trpc.useMutation(["match.acceptFriendReq"], {
+    onSuccess: () => {
+      utils.invalidateQueries(["match.getFriendRequests"]);
+    },
+  });
+  const declineFriendReqMutation = trpc.useMutation(
+    ["match.declineFriendReq"],
+    {
+      onSuccess: () => {
+        utils.invalidateQueries(["match.getFriendRequests"]);
+      },
+    }
+  );
+
+  const utils = trpc.useContext();
+
+  if (acceptDecline) {
+    return (
+      <div
+        className="flex justify-between items-center border rounded-3xl border-indigo-400 p-2 mb-4"
+        key={profileInput.id}
+      >
+        <div className="flex justify-start items-center">
+          <NextLink href={`profile/${profileInput.name}`}>
+            <img
+              className="h-16 w-16 rounded-full cursor-pointer"
+              src={profileInput.image || ""}
+            ></img>
+          </NextLink>
+          <div className="p-4"></div>
+          <div className="flex flex-col">
+            <NextLink href={`profile/${profileInput.name}`}>
+              <div className="underline cursor-pointer text-center md:text-left">
+                @{profileInput.name}
+              </div>
+            </NextLink>
+
+            <div className="text-sm text-center md:text-left">
+              {profileInput.firstName}
+            </div>
+            <div className="flex md:items-center flex-col md:flex-row">
+              {profileInput.tier && (
+                <div className="bg-gray-800 mt-2 pt-1 pb-1 pr-2 pl-2 rounded-lg text-sm mr-2 capitalize text-center md:text-left">
+                  {profileInput.tier.toLowerCase()}
+                </div>
+              )}
+              {profileInput.role && (
+                <>
+                  <div className="bg-gray-800 mt-2 pt-1 pb-1 pr-2 pl-2 rounded-lg text-sm mr-2 text-center md:text-left">
+                    {profileInput.role}
+                  </div>
+                </>
+              )}
+              {profileInput.fav_champion1 && (
+                <div className="bg-gray-800 mt-2 pt-1 pb-1 pr-2 pl-2 rounded-lg text-sm mr-2 text-center md:text-left">
+                  {profileInput.fav_champion1}
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-end items-center">
+          <div className="flex flex-col p-2 text-center">
+            <div className="text-center"></div>
+            <div>
+              <button
+                onClick={() => {
+                  if (profileInput.id && !acceptFriendReqMutation.isLoading) {
+                    acceptFriendReqMutation.mutate({
+                      requestInitiatorId: profileInput.id,
+                    });
+                  }
+                }}
+                type="button"
+                disabled={
+                  acceptFriendReqMutation.isLoading ||
+                  declineFriendReqMutation.isLoading
+                }
+                className="disabled:opacity-50 disabled:cursor-auto disabled:hover:bg-inherit text-indigo-400 border border-indigo-400 hover:bg-green-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-green-700 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center"
+              >
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span className="sr-only">Accept</span>
+              </button>
+            </div>
+            <div className="text-center">accept</div>
+          </div>
+          <div className="flex flex-col pt-2 pl-2 pb-2 text-center">
+            <div>
+              <button
+                onClick={() => {
+                  if (profileInput.id && !acceptFriendReqMutation.isLoading) {
+                    declineFriendReqMutation.mutate({
+                      requestInitiatorId: profileInput.id,
+                    });
+                  }
+                }}
+                type="button"
+                disabled={
+                  acceptFriendReqMutation.isLoading ||
+                  declineFriendReqMutation.isLoading
+                }
+                className="disabled:opacity-50 disabled:cursor-auto disabled:hover:bg-inherit text-indigo-400 border border-indigo-400 hover:bg-red-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-red-700 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center"
+              >
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span className="sr-only">Decline</span>
+              </button>
+            </div>
+            <div className="text-center">decline</div>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className="flex justify-between items-center border rounded-3xl border-indigo-400 p-2 mb-4"
+        key={profileInput.id}
+      >
+        <div className="flex justify-start items-center">
+          <NextLink href={`profile/${profileInput.name}`}>
+            <img
+              className="h-16 w-16 rounded-full cursor-pointer"
+              src={profileInput.image || ""}
+            ></img>
+          </NextLink>
+          <div className="p-4"></div>
+          <div className="flex flex-col">
+            <NextLink href={`profile/${profileInput.name}`}>
+              <div className="underline cursor-pointer text-center md:text-left">
+                @{profileInput.name}
+              </div>
+            </NextLink>
+
+            <div className="text-sm text-center md:text-left">
+              {profileInput.firstName}
+            </div>
+            <div className="flex md:items-center flex-col md:flex-row">
+              {profileInput.tier && (
+                <div className="bg-gray-800 mt-2 pt-1 pb-1 pr-2 pl-2 rounded-lg text-sm mr-2 capitalize text-center md:text-left">
+                  {profileInput.tier.toLowerCase()}
+                </div>
+              )}
+              {profileInput.role && (
+                <>
+                  <div className="bg-gray-800 mt-2 pt-1 pb-1 pr-2 pl-2 rounded-lg text-sm mr-2 text-center md:text-left">
+                    {profileInput.role}
+                  </div>
+                </>
+              )}
+              {profileInput.fav_champion1 && (
+                <div className="bg-gray-800 mt-2 pt-1 pb-1 pr-2 pl-2 rounded-lg text-sm mr-2 text-center md:text-left">
+                  {profileInput.fav_champion1}
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default ProfileCard;
--- src/pages/requests.tsx
+++ src/pages/requests.tsx
@@ -4,6 +4,7 @@ import { signIn } from "next-auth/react";
 import Navbar from "../components/Navbar";
 import NextLink from "next/link";
 import Head from "next/head";
+import ProfileCard from "../components/ProfileCard";
 
 const Requests: NextPage = () => {
   const { data: meData, isLoading } = trpc.useQuery(["user.me"]);
@@ -11,21 +12,21 @@ const Requests: NextPage = () => {
     ["match.getFriendRequests"]
   );
 
-  const acceptFriendReqMutation = trpc.useMutation(["match.acceptFriendReq"], {
-    onSuccess: () => {
-      utils.invalidateQueries(["match.getFriendRequests"]);
-    },
-  });
-  const declineFriendReqMutation = trpc.useMutation(
-    ["match.declineFriendReq"],
-    {
-      onSuccess: () => {
-        utils.invalidateQueries(["match.getFriendRequests"]);
-      },
-    }
-  );
+  // const acceptFriendReqMutation = trpc.useMutation(["match.acceptFriendReq"], {
+  //   onSuccess: () => {
+  //     utils.invalidateQueries(["match.getFriendRequests"]);
+  //   },
+  // });
+  // const declineFriendReqMutation = trpc.useMutation(
+  //   ["match.declineFriendReq"],
+  //   {
+  //     onSuccess: () => {
+  //       utils.invalidateQueries(["match.getFriendRequests"]);
+  //     },
+  //   }
+  // );
 
-  const utils = trpc.useContext();
+  // const utils = trpc.useContext();
 
   if (isLoading) {
     return (
@@ -76,135 +77,13 @@ const Requests: NextPage = () => {
           <h1 className="text-2xl text-indigo-200 font-semibold">
             My friend requests
           </h1>
-          <div className="mx-auto pt-4">
-            {friendsRequests?.map((req) => (
-              <div
-                className="flex justify-between items-center border rounded-3xl border-indigo-400 p-2 mb-4"
-                key={req.id}
-              >
-                <div className="flex justify-start items-center">
-                  <NextLink href={`profile/${req.requestInitiator.name}`}>
-                    <img
-                      className="h-16 w-16 rounded-full cursor-pointer"
-                      src={req.requestInitiator.image || ""}
-                    ></img>
-                  </NextLink>
-                  <div className="p-4"></div>
-                  <div className="flex flex-col">
-                    <NextLink href={`profile/${req.requestInitiator.name}`}>
-                      <div className="underline cursor-pointer text-center md:text-left">
-                        @{req.requestInitiator.name}
-                      </div>
-                    </NextLink>
-
-                    <div className="text-sm text-center md:text-left">
-                      {req.requestInitiator.firstName}
-                    </div>
-                    <div className="flex md:items-center flex-col md:flex-row">
-                      {req.requestInitiator.tier && (
-                        <div className="bg-gray-800 mt-2 pt-1 pb-1 pr-2 pl-2 rounded-lg text-sm mr-2 capitalize text-center md:text-left">
-                          {req.requestInitiator.tier.toLowerCase()}
-                        </div>
-                      )}
-                      {req.requestInitiator.role && (
-                        <>
-                          <div className="bg-gray-800 mt-2 pt-1 pb-1 pr-2 pl-2 rounded-lg text-sm mr-2 text-center md:text-left">
-                            {req.requestInitiator.role}
-                          </div>
-                        </>
-                      )}
-                      {req.requestInitiator.fav_champion1 && (
-                        <div className="bg-gray-800 mt-2 pt-1 pb-1 pr-2 pl-2 rounded-lg text-sm mr-2 text-center md:text-left">
-                          {req.requestInitiator.fav_champion1}
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                </div>
-                <div className="flex justify-end items-center">
-                  <div className="flex flex-col p-2 text-center">
-                    <div className="text-center"></div>
-                    <div>
-                      <button
-                        onClick={() => {
-                          if (
-                            req.requestInitiator.id &&
-                            !acceptFriendReqMutation.isLoading
-                          ) {
-                            acceptFriendReqMutation.mutate({
-                              requestInitiatorId: req.requestInitiator.id,
-                              requestTargetId: meData.id,
-                            });
-                          }
-                        }}
-                        type="button"
-                        disabled={
-                          acceptFriendReqMutation.isLoading ||
-                          declineFriendReqMutation.isLoading
-                        }
-                        className="disabled:opacity-50 disabled:cursor-auto disabled:hover:bg-inherit text-indigo-400 border border-indigo-400 hover:bg-green-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-green-700 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center"
-                      >
-                        <svg
-                          aria-hidden="true"
-                          className="w-5 h-5"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
-                        <span className="sr-only">Accept</span>
-                      </button>
-                    </div>
-                    <div className="text-center">accept</div>
-                  </div>
-                  <div className="flex flex-col pt-2 pl-2 pb-2 text-center">
-                    <div>
-                      <button
-                        onClick={() => {
-                          if (
-                            req.requestInitiator.id &&
-                            !acceptFriendReqMutation.isLoading
-                          ) {
-                            declineFriendReqMutation.mutate({
-                              requestInitiatorId: req.requestInitiator.id,
-                              requestTargetId: meData.id,
-                            });
-                          }
-                        }}
-                        type="button"
-                        disabled={
-                          acceptFriendReqMutation.isLoading ||
-                          declineFriendReqMutation.isLoading
-                        }
-                        className="disabled:opacity-50 disabled:cursor-auto disabled:hover:bg-inherit text-indigo-400 border border-indigo-400 hover:bg-red-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-red-700 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center"
-                      >
-                        <svg
-                          aria-hidden="true"
-                          className="w-5 h-5"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
-                        <span className="sr-only">Decline</span>
-                      </button>
-                    </div>
-                    <div className="text-center">decline</div>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
+          <div className="pt-4"> </div>
+          {friendsRequests?.map((req) => (
+            <ProfileCard
+              acceptDecline={true}
+              profileInput={req.requestInitiator}
+            ></ProfileCard>
+          ))}
         </div>
       </>
     );
